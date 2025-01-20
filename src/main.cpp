ec70146967413bcde0b80c347f8d43cb8d7a1fcd
Cheeseworks <bluewitherer2422@gmail.com>
2025-01-20 09:31:49 -0500

    cord

--- src/main.cpp
+++ src/main.cpp
@@ -135,7 +135,7 @@ void scanForUserBadge(CCMenu *cell_menu, float size, auto pointer, int itemID)
 					} else {
                     	if (getThisMod->getSettingValue<bool>("console")) log::debug("Fetched badge {} remotely", ogdWebResUnwr.c_str());
 					
-                    	if (cell_menu && pointer) {
+                    	if (cell_menu != nullptr && pointer != nullptr) {
 							setUserBadge(ogdWebResUnwr, cell_menu, size, pointer);
 						};
 					};
